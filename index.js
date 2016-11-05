f0b86ade90655c44e7eb692ec43c1fcdcfa34bce
Karaggeorge <karaggeorge0@gmail.com>
2016-11-05 13:12:18 -0400

    changes

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
+import Nutrients from './modules/Nutrients'
 import About from './modules/About'
 import Repos from './modules/Repos'
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
+    <Route path="/nut" component={Nutrients}/>
   </Router>
 ), document.getElementById('app'))
