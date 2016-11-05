3b7cd1c3f00f7c9ea57864cd118b555aeb133c4b
Karaggeorge <karaggeorge0@gmail.com>
2016-11-05 12:01:51 -0400

    first code

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+import App from './modules/App'
+import About from './modules/About'
+import Repos from './modules/Repos'
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+), document.getElementById('app'))
